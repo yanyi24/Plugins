27a6634b6d5aea22a4cc65088411b35f0f837079
yanyi <yanyi9399@outlook.com>
2019-01-02 11:32:57 +0800

    倒计时首次上传

---
+++ src/countdown/countdown.js
@@ -0,0 +1,224 @@
+// import './countdown_flex.less';
+// import './countdown_IE.less';
+
+function delay_script(A) {
+	var B = document.createElement("script")
+		, C = "src"
+		, D = "text/javascript";
+	B.setAttribute(C, A);
+	B.setAttribute("type", D);
+	document.body.appendChild(B);
+	return B
+}
+function hasJQ(){
+	if (typeof jQuery != 'undefined') {
+		return true;
+	} else {
+		delay_script("https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.1/jquery.min.js");
+		return true;
+	}
+}
+class yCountdown{
+	constructor(options){
+		this.config = $.extend({
+			el: $('.y-countdown'),
+			hasDay : false,
+			hasMonth : false,
+			hasYear : false,
+			flag: true,
+			end: 24,
+			loop: true,
+			callback: null,
+			endStampName: 'endStamp',
+			hrsTenthEle:  '[data-yCountdown="hrsTenth"]',
+			hrsEle:       '[data-yCountdown="hrs"]',
+			minsTenthEle: '[data-yCountdown="minsTenth"]',
+			minsEle:      '[data-yCountdown="mins"]',
+			secsTenthEle: '[data-yCountdown="secsTenth"]',
+			secsEle:      '[data-yCountdown="secs"]',
+			daysTenthEle: '[data-yCountdown="daysTenth"]',
+			daysEle:      '[data-yCountdown="days"]',
+			flagTxt: {
+				d: 'DAY',
+				h: 'HRS',
+				min: 'MINS',
+				s: 'SECS'
+			},
+			template: `
+			<div class="countdown" data-ycountdown="yCountdown">
+				<div class="d">
+					<div>
+						<span data-ycountdown="daysTenth">0</span><span data-ycountdown="days">0</span>
+					</div>
+					<p class="txt"></p>
+				</div>
+				<i class="d">:</i>
+				<div class="h">
+					<div>
+						<span data-ycountdown="hrsTenth">0</span><span data-ycountdown="hrs">0</span>
+					</div>
+					<p class="txt"></p>
+				</div>
+				<i>:</i>
+				<div class="min">
+					<div>
+						<span data-ycountdown="minsTenth">0</span><span data-ycountdown="mins">0</span>
+					</div>
+				<p class="txt"></p>
+				</div>
+				<i>:</i>
+				<div class="s">
+					<div>	
+						<span data-ycountdown="secsTenth">0</span><span data-ycountdown="secs">0</span>
+					</div>
+					<p class="txt"></p>
+				</div>
+			</div>
+			`,
+			style: `<style type="text/css" class="yconutdownstyle">.countdown{text-align:center;width:auto;height:auto;background-color:#fd8a2e;-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box;position:relative}.countdown>div{display:inline-block;vertical-align:top;height:auto}.countdown>div span{display:inline-block;width:30px;height:30px;text-align:center;line-height:30px;font-size:24px;color:#fd8a2e;background-color:#fff;-webkit-border-radius:2px;-moz-border-radius:2px;border-radius:2px;margin:0 2px}.countdown>div p{text-align:center;font-size:14px;color:#000;width:100%;margin:0;padding:0}.countdown>i{display:inline-block;height:30px;font-size:22px;color:#fff;line-height:30px;vertical-align:top;font-style:normal}</style>`
+		}, options || {});
+		this.hours = 0;
+		this.now = 0;
+		
+		this.timer = null;
+		
+	}
+	calcEnding(){
+		const end = this.config.end;
+		if (typeof end == 'number') {
+			this.hours = end;
+			this.endStamp = new Date().getTime() + new Date(this.hours * 60 * 60 * 1000).getTime();
+		}else if(typeof end == 'string' && (end.indexOf('/') > -1 || end.indexOf(',') > -1) && end.length === 10){
+			this.endStamp = new Date(end).getTime();
+			this.hours = (this.endStamp - new Date().getTime()) / (60*60*1000);
+			this.config.loop = false;
+		}
+		if(this.hours > 24) this.config.hasDay = true;
+	}
+	setStorage(k,v){
+		if (window.localStorage) {
+			window.localStorage.setItem(k,v);
+		} else {
+			document.cookie = k + "=" + v;
+		}
+	}
+	getStorage(k){
+		if (window.localStorage) {
+			return window.localStorage.getItem(k) ? window.localStorage.getItem(k) : false;
+		} else {
+			document.cookie.split(';').forEach(function (v) {
+				if(v.indexOf(k) !== -1){
+					return v.split('=')[1];
+				}else{
+					return false;
+				}
+			});
+		}
+	}
+	removeStorage(k){
+		if (window.localStorage) {
+			localStorage.removeItem(k);
+		}else{
+			document.cookie = k + "=" + escape('') + ";expires=-1";
+		}
+	}
+	calcTimes(timeStamp){
+		let times = {};
+		times.years = parseInt(timeStamp / (1000*60*60*24*30*12));
+		times.months = parseInt(timeStamp / (1000*60*60*24*30) % 12);
+		times.days = parseInt(timeStamp / (1000*60*60*24) % 365 % 30);
+		times.hours = parseInt(timeStamp / (1000*60*60) % 24);
+		times.mins = parseInt(timeStamp / (1000*60) % 60);
+		times.secs = parseInt(timeStamp / 1000 % 60 % 60);
+		return times;
+	}
+	countdown() {
+		this.now = new Date().getTime();
+		const {
+			years,
+			months,
+			days,
+			hours,
+			mins,
+			secs
+		} = this.calcTimes(this.endStamp - this.now);
+		const cfg = this.config;
+		const el = cfg.el;
+		el.find(cfg.hrsTenthEle).text(hours >= 10 ? parseInt(hours / 10) : '0');
+		el.find(cfg.hrsEle).text(hours % 10);
+		el.find(cfg.minsTenthEle).text(mins >= 10 ? parseInt(mins / 10) : '0');
+		el.find(cfg.minsEle).text(mins % 10);
+		el.find(cfg.secsTenthEle).text(secs >= 10 ? parseInt(secs / 10) : '0');
+		el.find(cfg.secsEle).text(secs % 10);
+		if (cfg.hasDay) {
+			el.find(cfg.daysTenthEle).text(days >= 10 ? parseInt(days / 10) : '0');
+			el.find(cfg.daysEle).text(days % 10);
+		}
+		this.updateEndStamp();
+	}
+	updateEndStamp(){
+		const cfg = this.config;
+		const el = cfg.el;
+		if (!this.getStorage(cfg.endStampName) && this.timer !== null) { //没有cookie时
+			this.setStorage(cfg.endStampName, this.endStamp);
+		}else{
+			this.endStamp = parseInt(this.getStorage(cfg.endStampName) );
+			
+			if(this.now >= this.endStamp ){ //当前时间超过cookie设置值
+				if (cfg.loop) {
+					const overTime = this.now - this.endStamp;
+					const overHrs = overTime / (1000*60*60);
+					let needToAddTime = 0;
+					if(overHrs < this.hours){
+						needToAddTime = this.hours *60*60*1000;
+					}else {
+						needToAddTime = (parseInt(overHrs/this.hours) + 1) * this.hours *60*60*1000;
+					}
+					this.endStamp += needToAddTime; 
+					this.setStorage(cfg.endStampName, this.endStamp);
+				}else if (!cfg.loop) {
+					clearInterval(this.timer);
+					this.timer = null;
+
+					el.find(cfg.hrsTenthEle).text('0');
+					el.find(cfg.hrsEle).text('0');
+					el.find(cfg.minsTenthEle).text('0');
+					el.find(cfg.minsEle).text('0');
+					el.find(cfg.secsTenthEle).text('0');
+					el.find(cfg.secsEle).text('0');
+					if (cfg.hasDay) {
+					el.find(cfg.daysTenthEle).text('0');
+						el.find(cfg.daysEle).text('0');
+					}
+					if(typeof cfg.callback === 'function' && cfg.callback !== undefined){
+						cfg.callback();
+					}
+				}
+			}
+		}
+		
+	}
+	handleHtml(){
+		const config = this.config;
+		const {d,h,min,s} = config.flagTxt;
+		if (config.el) {
+			$('.yconutdownstyle').length === 0 && $('head').append(config.style);
+			config.el.append(config.template);
+			if (config.flag) {
+				config.el.find('.d .txt').text(d);
+				config.el.find('.h .txt').text(h);
+				config.el.find('.min .txt').text(min);
+				config.el.find('.s .txt').text(s);
+			}
+			!config.hasDay && config.el.find('.d').remove();
+		}
+	}
+	init(){
+		this.calcEnding();
+		this.handleHtml();
+		this.timer = setInterval(function() {
+			this.countdown();
+		}.bind(this), 1000);
+	}
+}
+module.exports = yCountdown;
---
+++ webpack.config.js
@@ -0,0 +1,172 @@
+const path = require('path');
+const webpack = require("webpack");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+// const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+
+// const ExtractLess = new ExtractTextPlugin({
+//     filename: 'css/[name].css?v=[hash:8]',
+//     allChunks: true
+// });
+
+let obj = {
+
+  'c': {
+    distDirecotry: 'countdown',
+    dist: 'dist',
+    entry: {
+      'jquery': './node_modules/jquery/dist/jquery.min.js',
+      'countdown.jquery': './src/countdown/countdown.js'
+    },
+    html: (
+      new HtmlWebpackPlugin({
+        filename: 'demo.html', //配置输出文件名和路径
+        template: './src/countdown/demo.html', //配置文件模板
+        chunksSortMode: 'manual', // 设置排序
+        chunks: ['jquery', 'countdown.jquery'], // 设置js
+        minify: true,
+        cache: false,
+        inject: 'body'
+      })
+    )
+  },
+
+};
+
+let o = obj.c;
+
+let distDirecotry = 'countdown'; //bu
+let dist = o.dist;
+
+module.exports = {
+  entry: o.entry,
+  output: {
+    path: path.resolve(__dirname, distDirecotry),
+    filename: dist + '/[name].js?v=[hash:8]',
+    publicPath: "/"
+  },
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new UglifyJsPlugin({}),
+      new OptimizeCSSAssetsPlugin({})
+    ],
+  },
+
+  module: {
+    rules: [{
+        test: require.resolve('./node_modules/jquery/dist/jquery.min.js'),
+        use: [{
+          loader: 'expose-loader',
+          options: 'jQuery'
+        }, {
+          loader: 'expose-loader',
+          options: '$'
+        }]
+      },
+      {
+        test: require.resolve('./src/countdown/countdown.js'),
+        use: [{
+          loader: 'expose-loader',
+          options: 'yCountdown'
+        }]
+      },
+      {
+        test: /\.jsx?$/,
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        use: [{
+            'loader': 'babel-loader',
+            options: {
+              presets: ['env']
+            },
+          }
+        ],
+        exclude: /node-modules/
+      },
+      {
+        'test': /\.(less|css)$/,
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              "plugins": (loader) => [
+                require("autoprefixer")({
+                  browsers: 'cover 99.5%'
+                })
+              ]
+            }
+          },
+          {
+            loader: 'less-loader'
+          }
+        ]
+      },
+      {
+        'test': /\.(?:png|jpe?g|gif|svg|eot|woff|woff2|ttf)$/i,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: dist + '/static/'
+          }
+        }]
+      },
+    ]
+  },
+
+  //代码模块路径解析的配置
+  resolve: {
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, 'src')
+    ],
+    extensions: ['.wasm', '.mjs', '.js', '.json', '.jsx']
+  },
+  //插件
+  plugins: [
+    //使用 uglify-webpack-plugin 来压缩JS代码
+    new UglifyJsPlugin({
+      test: /\.(js)$/i,
+      parallel: 4,
+      sourceMap: false,
+      uglifyOptions: {
+        compress: true,
+        minify: true
+      }
+    }),
+    o.html,
+    new MiniCssExtractPlugin({
+      filename: "[name].css?v=[hash:6]",
+      chunkFilename: "[name].css"
+    }),
+
+    new OptimizeCSSAssetsPlugin({
+      assetNameRegExp: /\.(less|css)$/i,
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: {
+        discardComments: {
+          removeAll: true
+        }
+      },
+      canPrint: true
+    }),
+
+    // new BundleAnalyzerPlugin({ analyzerPort: 8019 })
+  ]
+};
